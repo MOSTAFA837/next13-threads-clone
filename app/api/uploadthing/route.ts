e131cded156994d83c78cb87e899cc5af75db807
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-08-27 21:59:43 +0200

    fix uploadthing route

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,3 +1,6 @@
+// Resource: https://docs.uploadthing.com/nextjs/appdir#create-a-nextjs-api-route-using-the-filerouter
+// Copy paste (be careful with imports)
+
 import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
@@ -6,11 +9,3 @@ import { ourFileRouter } from "./core";
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
-
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: "4mb", // Set desired value here
-    },
-  },
-};
