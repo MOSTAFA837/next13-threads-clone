e9caae44fef5373f62e388e89aec69e9f98dc314
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-08-27 22:17:20 +0200

    fix imports

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,17 +1,22 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 async function page() {
-  const authUser = await currentUser();
-  const userInfo = {};
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: authUser?.id,
+    id: user.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || authUser?.username,
-    name: userInfo?.name || authUser?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || authUser?.imageUrl,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
   };
 
   return (
